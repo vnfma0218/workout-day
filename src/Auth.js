f00d19fa03962762942c9600301912c23a670be3
pooreum <vnfma0218@naver.com>
2021-09-22 15:11:27 +0900

    hover message add

--- src/Auth.js
+++ src/Auth.js
@@ -9,10 +9,26 @@ export default function Auth() {
       <div className={classes.form__container}>
         <div className={classes.form__container_content}>
           <h1 className={classes.form__title}>Thank you for visiting</h1>
-          <h1 className={classes.authMode__title}>Login</h1>
-          <div className={classes.form}>
-            <label htmlFor='workout-mode'>운동모드</label>
-            <input type='checkbox' />
+          <h1 className={classes.authMode__title}>회원가입</h1>
+          <div className={classes.workout__mode_container}>
+            <div className={classes.workout__mode}>
+              <label htmlFor='workout-mode'>
+                운동모드<strong>?</strong>
+                <span className={classes.workout__mode_text}>
+                  기본적인 운동을 기록하고 확인할 수 있습니다.
+                </span>
+              </label>
+              <input type='checkbox' />
+            </div>
+            <div className={classes.diet__mode}>
+              <label htmlFor='workout-mode'>
+                다이어트모드<strong>?</strong>
+              </label>
+              <span className={classes.diet__mode_text}>
+                목표 몸무게를 설정하고 식단과 몸무게를 기록할 수 있습니다
+              </span>
+              <input type='checkbox' />
+            </div>
           </div>
           <form className={classes.auth__form}>
             <div className={classes.form__control}>
