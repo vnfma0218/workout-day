69fe345cb0714d5e21f4209c7c139cc339423eaf
yjw <jwjwhappy92@gmail.com>
2021-09-22 13:05:22 +0900

    wrapper css edit

--- src/shared/UIElement/Wrapper.js
+++ src/shared/UIElement/Wrapper.js
@@ -5,29 +5,23 @@ import classes from './Wrapper.module.css';
 export default function Wrapper(props) {
   const mode = useContext(ModeContext);
 
-  // <div
-  //       className={
-  //         mode.isDietMode
-  //           ? classes.container__dietmode
-  //           : classes.container__normalmode
-  //       }
-  //     ></div>
-
   return (
-    <div
-      className={
-        mode.isDietMode
-          ? `${classes.wrapper__dietmode} ${props.className}`
-          : `${classes.wrapper__normalmode} ${props.className}`
-      }
-    >
-      {props.children}
-      <button className={classes.dietMode} onClick={mode.onDietMode}>
-        DietMode
-      </button>
-      <button className={classes.NormalMode} onClick={mode.onNormalMode}>
-        NormalMode
-      </button>
+    <div className={classes.wrapper}>
+      <div
+        className={
+          mode.isDietMode
+            ? `${classes.wrapper__dietmode} ${props.className}`
+            : `${classes.wrapper__normalmode} ${props.className}`
+        }
+      >
+        {props.children}
+        <button className={classes.dietMode} onClick={mode.onDietMode}>
+          DietMode
+        </button>
+        <button className={classes.NormalMode} onClick={mode.onNormalMode}>
+          NormalMode
+        </button>
+      </div>
     </div>
   );
 }
