f760448386eec64133bcff97f27f86b73ae27973
yjw <jwjwhappy92@gmail.com>
2021-09-30 15:37:25 +0900

    edit

---
+++ src/shared/UIElement/Map.js
@@ -0,0 +1,13 @@
+/* global kakao */
+import React from 'react'
+
+
+export default function Map() {
+    return (
+        <div
+      className="map"
+      style={{ width: "500px", height: "500px" }}
+    ></div>
+ 
+    )
+}
